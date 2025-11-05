c5378106df44cce0d9d0c7566e48c7e01c4b3881
Bulhan-Maalin <maalin.622140@swl.k12.oh.us>
2025-11-05 08:54:25 -0500

    Finshed task 4

--- src/Task4.java
+++ src/Task4.java
@@ -3,5 +3,18 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        double userInput;
+        boolean YN;
+        double totalPrice = 0;
+
+        do {
+            userInput = InputHelper.getRangedDouble(scan, "Enter the item price: $", .50, 9.99);
+            scan.nextLine();
+            totalPrice += userInput;
+            YN = InputHelper.getYNConfirm(scan, "Do you want to add another item? [Y/N]");
+
+        } while (YN);
+
+        System.out.printf("Total Cost in $:     " + "%.2f%n", totalPrice);
     }
 }
