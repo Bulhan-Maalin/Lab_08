69e641aaccbc14e1e6882a0016f4d614efcacd21
Bulhan-Maalin <maalin.622140@swl.k12.oh.us>
2025-11-07 08:26:26 -0500

    done

--- src/Task6.java
+++ src/Task6.java
@@ -3,21 +3,12 @@ import java.util.Scanner;
 public class Task6 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double userCelsius = 0;
-        userCelsius = celsiusToFahrenheit(scan, userCelsius);
-        System.out.println(userCelsius);
+        double celsius = 0;
+        celsius = celsiusToFahrenheit(scan, celsius);
+        System.out.printf("Your temperature in degrees fahrenheit is:    %.2f%n", celsius);
     }
-        public static double celsiusToFahrenheit(Scanner scan, double Celsius) {
-            double userCelsius = 0;
-            boolean valid = false;
-            double userFahrenheit;
-
-            userCelsius = InputHelper.getDouble(scan, "Enter a temperature in Celsius:");
-
-            userFahrenheit = (userCelsius * 1.8) + 32;
-
-            System.out.printf("You temperature in degrees Fahrenheit is:    " + "\"%.2f%n\"", userFahrenheit);
-
-            return userCelsius;
-    }
+        public static double celsiusToFahrenheit(Scanner scan, double celsius) {
+            celsius = InputHelper.getDouble(scan, "Please enter a temperature in celsius.");
+            return (celsius * 1.8) + 32.0;
+        }
 }
